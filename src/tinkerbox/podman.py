d6c91e5dc679f331855243abe1491ff0212e0147
Ildar Akhmetgaleev <akhilman@gmail.com>
2026-08-31 19:57:22 +0700

    feat: do not capture podman output

--- src/tinkerbox/podman.py
+++ src/tinkerbox/podman.py
@@ -5,7 +5,21 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 
-def run_podman(*args: str) -> str:
+def run_podman(*args: str):
+    """
+    raises subprocess.CalledProcessError on non-zero exit
+    """
+    command = ["podman", *args]
+    logger.debug("$ %s", shlex.join(command))
+
+    subprocess.run(
+        command,
+        text=True,
+        check=True,
+    )
+
+
+def run_podman_capture(*args: str) -> str:
     """
     raises subprocess.CalledProcessError on non-zero exit
     """
